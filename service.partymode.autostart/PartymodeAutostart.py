f91be7e2a2b67eea636bd6b32ae32830627d3c9a
Lukas Elsner <open@mindrunner.de>
2012-10-31 15:23:43 +0100

    v1.0.0
    bugfixes
    autostart when idle

--- service.partymode.autostart/PartymodeAutostart.py
+++ service.partymode.autostart/PartymodeAutostart.py
@@ -9,18 +9,33 @@ v0.0.2
 import xbmc
 import xbmcgui
 import xbmcaddon
+import sched
 import time
+
+s = sched.scheduler(time.time, time.sleep)
+
+def ontimer():
+    interval = Addon.getSetting('interval')
+    isplaying = xbmc.Player().isPlaying()
+    if isplaying == 0:
+        xbmc.executebuiltin("XBMC.PlayerControl(PartyMode)")
+    s.enter(int(interval), 1, ontimer, ())
 
 if __name__ == '__main__':
     addon_id = "service.partymode.autostart"
     Addon = xbmcaddon.Addon(addon_id)
     enable = Addon.getSetting('enable')
     delay = Addon.getSetting('delay')
-    fullscreen = Addon.getSetting('fullscreen')
-    fullscreendelay = Addon.getSetting('fullscreendelay')
+    fs = Addon.getSetting('fs')
+    fsdelay = Addon.getSetting('fsdelay')
+    idletimer = Addon.getSetting('idletimer')
+    interval = Addon.getSetting('interval')
     if enable == "true":
         time.sleep(int(delay))
         xbmc.executebuiltin("XBMC.PlayerControl(PartyMode)")
-        if fullscreen == "true":
-            time.sleep(int(fullscreendelay))
+        if fs == "true":
+            time.sleep(int(fsdelay))
             xbmc.executebuiltin("XBMC.ActivateWindow(visualisation)")
+        if idletimer == "true":
+            s.enter(int(interval), 1, ontimer, ())
+            s.run()
